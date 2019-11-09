f6481fdb5498fbce6e6fabdd91f31f82d2ff990e
Hannah Harrington <hannahharrington@outlook.com>
2019-11-09 21:16:01 +0000

    Automatically backed up by Learn

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  GENRES = []
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre unless GENRES.any? { |g| g == genre }
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
